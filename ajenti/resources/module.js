41c648d5848c0c8f89cb10ce7b680a71534959e3
Toni Stoyanov <stoyanov.toni@gmail.com>
2026-08-26 09:12:13 +0300

    Add secure Ajenti integration: ajenti/resources/module.js

---
+++ ajenti/resources/module.js
@@ -0,0 +1,11 @@
+angular.module('ajenti.frigotehnica', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view/frigotehnica-tunnel', {
+      templateUrl: '/frigotehnica:resources/view.html',
+      controller: 'FrigotehnicaTunnelController'
+    });
+  }])
+  .controller('FrigotehnicaTunnelController', ['$scope', 'pageTitle', function ($scope, pageTitle) {
+    pageTitle.set('Cloudflare Tunnel');
+  }]);
+
