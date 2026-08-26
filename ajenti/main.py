bc17413a303fe0b92224b6e4d5c3625cebc5fc12
Toni Stoyanov <stoyanov.toni@gmail.com>
2026-08-26 09:12:10 +0300

    Add secure Ajenti integration: ajenti/main.py

---
+++ ajenti/main.py
@@ -0,0 +1,19 @@
+from jadi import component
+
+from aj.plugins.core.api.sidebar import SidebarItemProvider
+
+
+@component(SidebarItemProvider)
+class FrigotehnicaSidebarItem(SidebarItemProvider):
+    def __init__(self, context):
+        self.context = context
+
+    def provide(self):
+        return [{
+            'attach': 'category:tools',
+            'name': 'Cloudflare Tunnel',
+            'icon': 'cloud',
+            'url': '/view/frigotehnica-tunnel',
+            'children': [],
+        }]
+
